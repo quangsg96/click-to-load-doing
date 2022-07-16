b01e84b688e8b15b227cff08f5c538383f1b4021
javascriptguy79 <jsmorgen735@gmail.com>
2022-07-16 14:47:08 +0700

    click to load doing

--- .history/assets/interaction_20220716144524.js
+++ .history/assets/interaction_20220716144524.js
@@ -8,14 +8,14 @@ document.getElementById('btn').onclick = function() {
   xhttp.send();
 }
 
-document.getElementById('btn2').onclick = function() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById('wrapper__part2').innerHTML = this.responseText;
-  }
-  xhttp.open("GET", "https://quangsg96.github.io/index2/");
-  xhttp.send();
-}
+// document.getElementById('btn2').onclick = function() {
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onload = function() {
+//     document.getElementById('wrapper__part2').innerHTML = this.responseText;
+//   }
+//   xhttp.open("GET", "https://quangsg96.github.io/index2/");
+//   xhttp.send();
+// }
 
 // ============ JQUERY ============
 // $(document).ready(function() {
